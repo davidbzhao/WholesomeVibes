552fdeab80ab55e7891b0f3474a34cec8c2581b5
David Zhao <davidzhao058@gmail.com>
2018-09-23 10:48:43 -0400

    Make cards show up on search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,73 @@
-import { Stitch } from 'mongodb-stitch-browser-sdk'
- 
-function initializeAndLogin() {
-  const client = Stitch.initializeDefaultAppClient('remhek-zuloy');
-  client.callFunction("testFunction", ["Hello world!"]).then(echoedResult => {
-    document.getElementsByTagName("body")[0].innerHTML = `<p>Echoed result: ${echoedResult.arg}</p>`;
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+let client;
+
+const initialize = () => {
+  client = Stitch.initializeDefaultAppClient('remhek-zuloy');
+}
+
+
+const createCallToAction = (org, text) => {
+  const urlMatches = text.match("(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?");
+  let url = '';
+  if (urlMatches && urlMatches.length > 0){
+    url += urlMatches[0];
+  }
+  const template = `<div class="coa">
+    <div class="coa-logo">
+      <div class="coa-logo-container">
+        <!--<img src="">-->
+      </div>
+    </div>
+    <div class="coa-content">
+      <h3>${org}</h3>
+      <p>${text}</p>
+      <a href="${url}">${url}</a>
+    </div>
+  </div>`;
+  return template;
+}
+
+const clearCategories = () => {
+  $('.coa').remove();
+}
+
+const submitQuery = () => {
+  clearCategories();
+  client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+    client.callFunction("searchEvent", [$('#search-bar').text()]).then(result => {
+      console.log(result);
+      if (result) {
+        const tweets = result.tweets;
+        console.log(tweets.length);
+        for (let cnt = 0; cnt < tweets.length; cnt++) {
+          const userName = tweets[cnt].user_name;
+          const text = tweets[cnt].text;
+          let category = tweets[cnt].category;
+          if (category != 'volunteer' && category != 'donations') {
+            category = 'other';
+          }
+          const callToActionCard = createCallToAction(userName, text);
+          console.log(callToActionCard);
+          $('#' + category).append(callToActionCard);
+        }
+      }
+    });
   });
 }
 
-window.onload = initializeAndLogin;
+const makeSectionTitlesAppear = () => {
+  $('.coa-group-title').css('opacity', '0.54');
+}
+
+$('#search-button').click(function() {
+  animateSearch();
+  submitQuery();
+  makeSectionTitlesAppear();
+});
+
+function animateSearch(){
+	const searchContainer = document.getElementsByClassName("search-container")[0];
+	searchContainer.classList.add("squashed-search");
+}
+
+initialize();
